581234f5f7e97081305f7b95313fd024abea9be8
Julfikar H Dinar <42842584+JulfikarHDinar@users.noreply.github.com>
2021-03-10 01:43:44 +0600

    Project005T

---
+++ TBashaBari/TBashaBari/Models/TenantRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TBashaBari.Models
+{
+    public class TenantRequest
+    {
+        [Key]
+        public int RequestId { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Email is not valid")]
+        [DisplayName("Email")]
+        public String TenantEmail { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 1, ErrorMessage = "Maximum text length 60")]
+        [DisplayName("Request")]
+        public String RequestText { get; set; }
+
+        [Required]
+        [DisplayName("Time")]
+        public String RequestTime { get; set; }
+
+        [StringLength(60, MinimumLength = 1, ErrorMessage = "Maximum text length 60")]
+        [DisplayName("Comment")]
+        public String CommentOnRequestText { get; set; }
+
+    }
+}
---
+++ TBashaBari/TBashaBari/WebConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TBashaBari
+{
+    public static class WebConstant
+    {
+        public static string OwnerRole = "Owner";
+        public static string TenantRole = "Tenant";
+    }
+}
