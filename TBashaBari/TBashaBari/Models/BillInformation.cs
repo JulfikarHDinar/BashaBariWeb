ebf082313f3b5aaff05299e0c0c871c4544cc749
Julfikar H Dinar <42842584+JulfikarHDinar@users.noreply.github.com>
2021-03-19 21:02:16 +0600

    Bill info MVC added
    model, view controller for bill info are added.. update your database

---
+++ TBashaBari/TBashaBari/Models/BillInformation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TBashaBari.Models
+{
+    public class BillInformation
+    {
+        [Key]
+        public int BillId { get; set; }
+
+        [Required]
+        public String OwnerEmail { get; set; }
+
+        [Required]
+        public String TenantEmail { get; set; }
+
+        [Required]
+        public String BillTime { get; set; }
+
+        [Required]
+        public String WaterAmount { get; set; }
+
+        [Required]
+        public String WaterPaid { get; set; }
+
+        [Required]
+        public String WaterVerified { get; set; }
+
+        [Required]
+        public String ElectricAmount { get; set; }
+
+        [Required]
+        public String ElectricPaid { get; set; }
+
+        [Required]
+        public String ElectricVerified { get; set; }
+
+        [Required]
+        public String RentAmount { get; set; }
+
+        [Required]
+        public String RentPaid { get; set; }
+
+        [Required]
+        public String RentVerified { get; set; }
+
+        [Required]
+        public String GasAmount { get; set; }
+
+        [Required]
+        public String GasPaid { get; set; }
+
+        [Required]
+        public String GasVerified { get; set; }
+    }
+}
