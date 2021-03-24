911e5113e35ff0f230cf1739c48aef5b19be74f2
Mainul029 <42498934+Mainul029@users.noreply.github.com>
2021-03-24 18:53:53 +0600

    databaseupdate

--- TBashaBari/TBashaBari/Controllers/DatabaseConnection.cs
+++ TBashaBari/TBashaBari/Controllers/DatabaseConnection.cs
@@ -18,9 +18,9 @@ namespace TBashaBari.Controllers
               Actual string looks like this: 
                              Dinar :-  "Data Source = \"DESKTOP-RUB62SE\\SQLEXPRESS\"; database = \"BashaBariWeb\"; integrated security = SSPI;";
                              saky  :-  "Data Source = \"DESKTOP-37APNEN\\SQLEXPRESS\"; database = \"BashaBariWeb\"; integrated security = SSPI;";
-                             Sajid :-  "Data Source = \"DESKTOP- Sajid tr server name de \\SQLEXPRESS\"; database = \"BashaBariWeb\"; integrated security = SSPI;";
+                             Sajid :-  "Data Source = \"DESKTOP-42FFPT6\\SQLEXPRESS\"; database = \"BashaBariWeb\"; integrated security = SSPI;";
              */
-            conn.ConnectionString = "Data Source = \"DESKTOP-37APNEN\\SQLEXPRESS\"; database = \"BashaBariWeb\"; integrated security = SSPI;";
+            conn.ConnectionString = "Data Source = \"DESKTOP-42FFPT6\\SQLEXPRESS\"; database = \"BashaBariWeb\"; integrated security = SSPI;";
             conn.Open();
             queryString.Connection = conn;
 
